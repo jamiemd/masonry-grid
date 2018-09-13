651352f50a465a118561b8cf9a5c9b185a2d7712
Jamie <jamiedomingo@hotmail.com>
2018-09-12 22:31:16 -0700

    refactored code for image rendering

--- src/Grid.js
+++ src/Grid.js
@@ -4,21 +4,17 @@ class Grid extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      // columns: [[30, 4], [8, 4], [2, 5, 7], [9], [90, 34], [9]],
       columns: [[], [], [], [], [], []],
       imagesArray: props.imagesArray
     };
   }
 
   componentDidMount() {
-    // console.log("this.state.imagesArray", this.state.imagesArray);
     let index = 0;
     let columns = this.state.columns;
     this.props.imagesArray.forEach(image => {
-      // console.log("image", image);
       index = this.getShortestColumn(columns);
       columns[index].push(image);
-      // console.log("this.state.columns", this.state.columns);
     });
     this.setState({ columns: columns });
   }
@@ -28,7 +24,6 @@ class Grid extends Component {
 
     for (let i = 0; i < columns.length; i++) {
       for (let j = 0; j < columns[i].length; j++) {
-        // console.log("columns[i][j].height", columns[i][j].height);
         columnTotals[i] += columns[i][j].height;
       }
     }
@@ -48,52 +43,20 @@ class Grid extends Component {
 
   render() {
     let columns = this.state.columns;
-    console.log("columns", columns);
 
     return (
       <div style={container}>
-        <div style={columnMain}>
-          {columns[0].map((image, i) => (
-            <div style={{ height: image.height, ...column }} key={i}>
-              hi
-            </div>
-          ))}
-        </div>
-        <div style={columnMain}>
-          {columns[1].map((image, i) => (
-            <div style={{ height: image.height, ...column }} key={i}>
-              hi
-            </div>
-          ))}
-        </div>
-        <div style={columnMain}>
-          {columns[2].map((image, i) => (
-            <div style={{ height: image.height, ...column }} key={i}>
-              hi
-            </div>
-          ))}
-        </div>
-        <div style={columnMain}>
-          {columns[3].map((image, i) => (
-            <div style={{ height: image.height, ...column }} key={i}>
-              hi
-            </div>
-          ))}
-        </div>
-        <div style={columnMain}>
-          {columns[4].map((image, i) => (
-            <div style={{ height: image.height, ...column }} key={i}>
-              hi
-            </div>
-          ))}
-        </div>
-        <div style={columnMain}>
-          {columns[5].map((image, i) => (
-            <div style={{ height: image.height, ...column }} key={i}>
-              hi
-            </div>
-          ))}
-        </div>
+        {columns.map((column, i) => (
+          <div style={columnMain} key={i}>
+            {column.map((image, i) => {
+              return (
+                <div style={{ height: image.height, ...singleColumn }} key={i}>
+                  hi
+                </div>
+              );
+            })}
+          </div>
+        ))}
       </div>
     );
   }
@@ -109,7 +72,7 @@ const columnMain = {
   margin: "10px"
 };
 
-const column = {
+const singleColumn = {
   width: "236px",
   marginBottom: "20px",
   backgroundColor: "gray"
